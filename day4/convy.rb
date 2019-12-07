429c169e52ed9bb5735592610f89357d172c1ec4
stubblyhead <stubblyhead@gmail.com>
2019-12-06 16:31:25 -0800

    part 2 complete

--- day4/convy.rb
+++ day4/convy.rb
@@ -1,12 +1,16 @@
+require 'pry'
+binding.pry
 candidates = (124075..580769)
 dupes = []
 valid = []
+valider = []
 
 # collect all numbers with at least one consecutive duplicate digit
 candidates.each do |i|
   i = i.to_s
-  dupes.push(i) if /.*(.)\1.*/.match(i)
+  dupes.push(i) if /(\d)\1/.match(i)
 end
+
 
 # collect all numbers from dupes[] that are equal to themselves after sorting digits in ascending order
 dupes.each do |i|
@@ -14,3 +18,11 @@ dupes.each do |i|
 end
 
 puts valid.length
+
+# prune out entries with only groups of 3+
+valid.each do |i|
+  i.gsub!(/(\d)\1{2,}/, 'X')
+  valider.push(i) if /(\d)\1/.match(i)
+end
+
+puts valider.length
