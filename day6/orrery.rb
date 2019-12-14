6322e3c81b7d94cf25ec14c0a582ad37a3dc96b0
stubblyhead <stubblyhead@gmail.com>
2019-12-13 19:05:42 -0800

    need to pass parent object, not just name.  also reading file wrong

--- day6/orrery.rb
+++ day6/orrery.rb
@@ -1,9 +1,13 @@
+require 'pry'
+binding.pry
+
 class Planet
   attr_reader :parent, :children, :orbit_count
 
-  def initialize(parent=nil)
+  def initialize(name, parent=nil)
     @parent = parent
     @children = []
+    @name = name
     if @parent
       @parent.add_child(self)
       @orbit_count = @parent.orbit_count + 1
@@ -20,13 +24,13 @@ end
 
 orrery = {}
 
-list = File.open('./input', :chomp=>true)
+list = File.readlines('./input', :chomp=>true)
 
 def traverse(list, system, planet, parent)
-  system[planet] = Planet.new(parent)
+  system[planet] = Planet.new(planet, parent)
   children_pairs = list.filter { |i| /^#{planet}/.match(i) }
-  children = children_pairs.map { |i| /\)(.*)/ }[1] }
-  children.each { |i| traverse(list, system, i, planet) }
+  children = children_pairs.map { |i| /\)(.*)/.match(i)[1] }
+  children.each { |i| traverse(list, system, i, system[planet]) }
 end
 
 total_orbits = 0
