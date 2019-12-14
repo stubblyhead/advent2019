b68846359f63b0b366210ba612187556f8beee67
stubblyhead <stubblyhead@gmail.com>
2019-12-14 15:03:02 -0800

    part 2 complete

--- day8/onion.rb
+++ day8/onion.rb
@@ -1,8 +1,5 @@
-require 'pry'
-binding.pry
-
 class Layer
-  attr_reader :count
+  attr_reader :count, :grid
   def initialize(w, h)
     @grid = Array.new(w) { Array.new(h) }
     @count = Hash.new(0)
@@ -53,3 +50,23 @@ layers.each do |l|
 end
 
 puts min_layer.count['1'] * min_layer.count['2']
+
+composite = Array.new(6) { Array.new(25) }
+composite.each_index do |i|
+  composite[i].each_index do |j|
+    curr_layer = 0
+    while true
+      if layers[curr_layer].grid[i][j] != '2'
+        composite[i][j] = layers[curr_layer].grid[i][j]
+      else
+        curr_layer += 1
+      end
+      break if composite[i][j]
+    end
+  end
+end
+values = ''
+composite.each { |i| values += i.join }
+final = Layer.new(6,25)
+final.populate(values)
+puts final
