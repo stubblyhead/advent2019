7aa6dae7bf32294e0f6e3e9f7b563e27ae79997c
stubblyhead <stubblyhead@gmail.com>
2019-12-17 23:01:19 -0800

    need to continue if slope is infinite

--- day10/orchard.rb
+++ day10/orchard.rb
@@ -1,32 +1,26 @@
-require 'pry'
-binding.pry
+# require 'pry'
+# binding.pry
 
-#grid = File.readlines('input', :chomp=>true).map { |s| s.split('') }
-
-grid =
-".#..#
-.....
-#####
-....#
-...##".lines(:chomp=>true).map{|s| s.split('') }
+grid = File.readlines('input', :chomp=>true).map { |s| s.split('') }
 
 asteroids = {}
 grid.each_index do |row|
   grid[row].each_index do |col|
     next if grid[row][col] == '.'
     asteroids[[row,col]] = []
-    grid.each_index do |i|
-      grid[i].each_index do |j|
-        next if i == row and j == col
-        next if grid[i][j] == '.'
-        if j == col
-          asteroids[[row,col]].push((row - i) * Float::INFINITY)
-        elsif j > col
-          m = ((i - row).to_f/(j - col))
+    grid.each_index do |t_row|
+      grid[t_row].each_index do |t_col|
+        next if t_row == row and t_col == col
+        next if grid[t_row][t_col] == '.'
+        if t_col == col
+          asteroids[[row,col]].push((row - t_row) * Float::INFINITY)
+          next
+        elsif t_col > col
+          m = ((t_row - row).to_f/(t_col - col))
         else
-          m = ((row - i).to_f/(col-j))
+          m = ((row - t_row).to_f/(col - t_col))
         end
-        if i > row
+        if t_col > col
           asteroids[[row,col]].push([m,:right])
         else
           asteroids[[row,col]].push([m,:left])
@@ -36,6 +30,6 @@ grid.each_index do |row|
   end
 end
 max_asteroids = 0
-asteroids.values.each { |i| max_asteroids = [max_asteroids, i.uniq.length] }
+asteroids.values.each { |i| max_asteroids = [max_asteroids, i.uniq.length].max }
 
 puts max_asteroids
